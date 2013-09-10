6653648bf0d88c6241998b06f0b62704a320771c
Delan Azabani <delan@azabani.com>
2013-09-10 12:00:30 +0800

    [264] allow multi-line user input using getchar()

--- delan.c
+++ delan.c
@@ -1,2 +1,2 @@
 #define X(a,b) (*i==a)-(*i==b)
-r;char M[30000],I[10000],U[10000],*m=M,*i=I,*u=U;main(){gets(i);gets(u);for(;*m+=X(43,45),m+=X(62,60),*i==44?*m=*u++:*i==46&&putchar(*m),*i;i++)if(*i==91&&!*m)do r+=X(91,93),i++;while(r);else if(*i==93&&*m)do r+=X(93,91),i--;while(r);}
+r;char M[30000],I[10000],*m=M,*i=I;main(c){gets(i);for(;*m+=X(43,45),m+=X(62,60),*i==44?c=getchar(),c!=-1&&(*m=c):*i==46&&putchar(*m),*i;i++)if(*i==91&&!*m)do r+=X(91,93),i++;while(r);else if(*i==93&&*m)do r+=X(93,91),i--;while(r);}
