f616765a13b38bfb36bb1ebbd772aa106c52ac76
Delan Azabani <delan@azabani.com>
2013-09-10 12:52:00 +0800

    [258] refactor, also swap between 91 and 93 with xor 6

--- delan.c
+++ delan.c
@@ -1,2 +1,3 @@
 #define X(a,b) (*i==a)-(*i==b)
-r;char M[30000],I[10000],*m=M,*i=I;main(c){gets(i);for(;*m+=X(43,45),m+=X(62,60),*i==44?c=getchar(),c<0||(*m=c):*i==46&&putchar(*m),*i;i++)if(*i==91&&!*m)do r+=X(91,93),i++;while(r);else if(*i==93&&*m)do r+=X(93,91),i--;while(r);}
+#define Z(a,o,p) if(*i==a&&o*m)do r+=X(a,(a^6)),i p;while(r);
+r;char M[30000],I[10000],*m=M,*i=I;main(c){gets(i);for(;*m+=X(43,45),m+=X(62,60),*i==44?c=getchar(),c<0||(*m=c):*i==46&&putchar(*m),*i;i++)Z(91,!,++)else Z(93,,--)}
