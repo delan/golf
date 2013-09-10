a47b565341d182db042317eb2544ae4032f555c6
Delan Azabani <delan@azabani.com>
2013-09-10 14:15:38 +0800

    [257] use ^|| instead of ==&&

--- delan.c
+++ delan.c
@@ -1,3 +1,3 @@
 #define X(a,b) (*i==a)-(*i==b)
 #define Z(a,o,p) if(*i==a&&o*m)do r+=X(a,(a^6)),i p;while(r);
-r;char M[30000],I[10000],*m=M,*i=I;main(c){gets(i);for(;*m+=X(43,45),m+=X(62,60),*i==44?c=getchar(),c<0||(*m=c):*i==46&&putchar(*m),*i;i++)Z(91,!,++)else Z(93,,--)}
+r;char M[30000],I[10000],*m=M,*i=I;main(c){gets(i);for(;*m+=X(43,45),m+=X(62,60),*i==44?c=getchar(),c<0||(*m=c):*i^46||putchar(*m),*i;i++)Z(91,!,++)else Z(93,,--)}
