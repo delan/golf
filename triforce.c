34e1692563810166657b2003041b9aa09fd31f90
Delan Azabani <delan@azabani.com>
2013-09-20 23:39:25 +0800

    use loop macro for initial space printing

--- triforce.c
+++ triforce.c
@@ -1,3 +1,3 @@
 #define P(x) putchar(x);
 #define L(x,q) for(s=1;s<x;s++)P(q)
-r,s;main(int X,char**Y){X=atoi(Y[1]);for(r=0;r<X;r++){for(s=X-r-1;s;s--)P(32)if(r<X/2)L(2+r*2,90)else{L(2+r*2-X,90)L((X-r)*2,32)L(2+r*2-X,90)}P(10)}}
+r,s;main(int X,char**Y){X=atoi(Y[1]);for(r=0;r<X;r++){L(X-r,32)if(r<X/2)L(2+r*2,90)else{L(2+r*2-X,90)L((X-r)*2,32)L(2+r*2-X,90)}P(10)}}
